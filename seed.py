4503d2178c6e55b3ea581dc4bbe40bb5d14cc2c2
Nika <n.tsitsqishvili@gmail.com>
2022-01-29 22:59:31 +0400

    final project

--- seed.py
+++ seed.py
@@ -1,13 +1,33 @@
 from faker import Faker
-from sqlalch import db, UserModel, PostModel
-from random import random
-fake = Faker()
-for i in range(1,200):
-    user = UserModel(username=fake.name(), email=fake.email())
-    db.session.add(user)
+from sqlalch import db, PoetsModel, PoemsModel
+poet_data = [{"poet_id": 1, "poet_name": "მირზა გელოვანი","birth_date": "2 მარტი, 1917","death_date": "ივლისი 1944"},
+             {"poet_id": 2,"poet_name": "გალაქტიონ ტაბიძე","birth_date": "17 ნოემბერი, 1891","death_date": "17 მარტი, 1959"},
+              {"poet_id": 3, "poet_name": "ლადო ასათიანი", "birth_date": "14 იანვარი, 1917","death_date": "23 ივნისი, 1943"},
+             {"poet_id": 4, "poet_name": "ლუკა რაზიკაშვილი", "birth_date": "27 მაისი, 1862","death_date": "27 ივლისი, 1915"},
+             {"poet_id": 5, "poet_name": "ტიციან ტაბიძე", "birth_date": "2 აპრილი, 1895","death_date": "16 დეკემბერი, 1937"}]
+
+poem_data = [{"poem_id": 1, "poem_title": "მე და ღამე","youtube_link":"https://www.youtube.com/watch?v=MRMDBQ7BlIg", "creator_id": 2},
+             {"poem_id": 2, "poem_title": "შენ","youtube_link":"https://www.youtube.com/watch?v=LYoI18YMUfc", "creator_id": 1},
+             {"poem_id": 3,"poem_title": "სალაღობო","youtube_link":"https://www.youtube.com/watch?v=PN9pG1eu_Bk", "creator_id": 3},
+             {"poem_id": 4,"poem_title": "ანანურთან","youtube_link":"https://www.youtube.com/watch?v=9bV1j2h0_Go", "creator_id": 5},
+             {"poem_id": 5,"poem_title": "სიკვდილი ყველას გვაშინებს","youtube_link": "https://www.youtube.com/watch?v=EAdgfELuA8M", "creator_id": 4}]
+
+for i in poet_data:
+    poet = PoetsModel(poet_name=i["poet_name"], birth_date=i["birth_date"], death_date=i["death_date"])
+    db.session.add(poet)
+    db.session.commit()
+for i in poem_data:
+    poet = PoemsModel(poem_title=i["poem_title"], youtube_link=i["youtube_link"], creator_id=i["creator_id"])
+    db.session.add(poet)
     db.session.commit()
 
-for i in range(1,200):
-    post = PostModel(title=fake.paragraph(nb_sentences=1), body=fake.text(), user_id=abs(i-200))
-    db.session.add(post)
-    db.session.commit()
+# fake = Faker()
+# for i in range(1,200):
+#     poet = PoetsModel(poet_name=fake.name(), birth_date=fake.date(),death_date=fake.date())
+#     db.session.add(poet)
+#     db.session.commit()
+#
+# for i in range(1,200):
+#     poem = PoemsModel(poem_title=fake.paragraph(nb_sentences=1), youtube_link=fake.paragraph(nb_sentences=1), creator_id=abs(i-200))
+#     db.session.add(poem)
+#     db.session.commit()
--- sqlalch.py
+++ sqlalch.py
@@ -1,73 +1,69 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api, reqparse, fields, marshal_with
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project1.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #ar gvinda modifikaciebis trrackingi
+app.config["JWT_SECRET_KEY"] = "our-secret-key" #must be changed
+
 db = SQLAlchemy(app)
-resource_fiels = {            #stringad rom gadaiqces rata postmenma waikitxos user getshi
+jwt = JWTManager(app)
+
+resource_user = {
     "id": fields.Integer,
     "username": fields.String,
-    "email": fields.String
+    "email": fields.String,
+    "password": fields.String
 }
-resource_posts = {
-    "id": fields.Integer,
-    "title": fields.String,
-    "body": fields.String,
-    "user_id": fields.Integer
+
+resource_poets = {            #stringad rom gadaiqces rata postmenma waikitxos user getshi
+    "poet_id": fields.Integer,
+    "poet_name": fields.String,
+    "birth_date": fields.String,
+    "death_date": fields.String
 }
+resource_poems = {
+    "poem_id": fields.Integer,
+    "poem_title": fields.String,
+    "youtube_link": fields.String,
+    "creator_id": fields.Integer
+}
+
+resource_register = {
+    "username": fields.String,
+    "email": fields.String,
+    "password": fields.String
+}
+
+registerparser = reqparse.RequestParser()
+registerparser.add_argument("username", type=str, help='user_name must be string')
+registerparser.add_argument("email", type=str, required=True, help='Email must be string')
+registerparser.add_argument("password", type=str, required=True, help='Email must be string')
 
 userparser = reqparse.RequestParser()
 userparser.add_argument("username", type=str, help='user_name must be string')
 userparser.add_argument("email", type=str, help='Email must be string')
+userparser.add_argument("password", type=str, help='Email must be string')
 
-postparser = reqparse.RequestParser()
-postparser.add_argument("id", type=int, help='Id must be integer')
-postparser.add_argument("title", type=str, help='Title must be string')
-postparser.add_argument("body", type=str, help='Body must be string')
-postparser.add_argument("user_id", type=int, help='User_id must be integer')
-class Post(Resource):
-    @marshal_with(resource_posts)
-    def get(self, post_id):
-        if post_id == 000:
-            return PostModel.query.all()
-        args = postparser.parse_args()
-        post = PostModel.query.filter_by(id=post_id).first()
-        return post
+poetparser = reqparse.RequestParser()
+poetparser.add_argument("poet_id", type=int, help='Id must be integer')
+poetparser.add_argument("poet_name", type=str, help='Poet_name must be string')
+poetparser.add_argument("birth_date", type=str, help='Birth date must be string')
+poetparser.add_argument("death_date", type=str, help='Death date must be string')
 
-    @marshal_with(resource_posts)
-    def post(self, post_id):
-        args = postparser.parse_args()
-        post = PostModel(title=args["title"], body=args["body"],user_id=args["user_id"])
-        db.session.add(post)
-        db.session.commit()
-        return "Item has been inserted!"
-
-    @marshal_with(resource_posts)
-    def put(self, post_id):
-        args = postparser.parse_args()
-        post = PostModel.query.filter_by(id=post_id).first()
-        if post == None:
-            post = PostModel(title=args["title"], body=args["body"], user_id=args["user_id"])
-        else:
-            post.title = args["title"]
-            post.body = args["body"]
-            post.user_id = args["user_id"]
-        db.session.add(post)
-        db.session.commit()
-        return "updated"
-
-    # @marshal_with(resource_posts)
-    def delete(self, post_id):
-        post = PostModel.query.filter_by(id=post_id).first()
-        db.session.delete(post)
-        db.session.commit()
-        return f"Post with id {post_id} has been deleted"
+poemparser = reqparse.RequestParser()
+poemparser.add_argument("poem_id", type=int, help='Id must be integer')
+poemparser.add_argument("poem_title", type=str, help='Title must be string')
+poemparser.add_argument("youtube_link", type=str, help='Body must be string')
+poemparser.add_argument("creator_id", type=int, help='User_id must be integer')
 
 class User(Resource):
-    @marshal_with(resource_fiels)
+    @marshal_with(resource_user)
+    # @jwt_required()
     def get(self, user_id):
         if user_id == 999:
             return UserModel.query.all()
@@ -75,58 +71,182 @@ class User(Resource):
         user = UserModel.query.filter_by(id=user_id).first()
         return user
 
-    @marshal_with(resource_fiels)
+    @marshal_with(resource_user)
+    @jwt_required()
     def post(self, user_id):
         args = userparser.parse_args()
-        user = UserModel(username=args["username"], email=args["email"])
+        password = generate_password_hash(args["password"])
+        user = UserModel(username=args["username"],email=args["email"], password=password)
         db.session.add(user)
         db.session.commit()
         return "inserted"
 
-    @marshal_with(resource_fiels)
+    @marshal_with(resource_user)
+    # @jwt_required()
     def put(self, user_id):
         args = userparser.parse_args()
         user = UserModel.query.filter_by(id=user_id).first()
+        password = generate_password_hash(args["password"])
         if user == None:
-            user = UserModel(username=args["username"], email=args["email"])
+            user = UserModel(username=args["username"], email=args["email"], password=password)
         else:
             user.username = args["username"]
             user.email = args["email"]
+            user.password = password
         db.session.add(user)
         db.session.commit()
         return "updated"
 
-    # @marshal_with(resource_fiels)
+    # @marshal_with(resource_user)
+    # @jwt_required()
     def delete(self, user_id):
         user = UserModel.query.filter_by(id=user_id).first()
         db.session.delete(user)
         db.session.commit()
         return f"User with id {user_id} has been deleted"
 
+class Poem(Resource):
+    @marshal_with(resource_poems)
+    def get(self, poem_id):
+        if poem_id == 000:
+            return PoemsModel.query.all()
+        args = poemparser.parse_args()
+        poem = PoemsModel.query.filter_by(poem_id=poem_id).first()
+        return poem
+
+    @marshal_with(resource_poems)
+    @jwt_required()
+    def post(self, poem_id):
+        args = poemparser.parse_args()
+        poem = PoemsModel(poem_title=args["poem_title"], youtube_link=args["youtube_link"],creator_id=args["creator_id"])
+        db.session.add(poem)
+        db.session.commit()
+        return "Item has been inserted!"
+
+    @marshal_with(resource_poems)
+    @jwt_required()
+    def put(self, poem_id):
+        args = poemparser.parse_args()
+        poem = PoemsModel.query.filter_by(poem_id=poem_id).first()
+        if poem == None:
+            poem = PoemsModel(poem_title=args["poem_title"], youtube_link=args["youtube_link"],creator_id=args["creator_id"])
+        else:
+            poem.poem_title = args["poem_title"]
+            poem.youtube_link = args["youtube_link"]
+            poem.creator_id = args["creator_id"]
+        db.session.add(poem)
+        db.session.commit()
+        return "updated"
+
+    # @marshal_with(resource_poems)
+    @jwt_required()
+    def delete(self, poem_id):
+        poem = PoemsModel.query.filter_by(poem_id=poem_id).first()
+        db.session.delete(poem)
+        db.session.commit()
+        return f"Poem with id {poem_id} has been deleted"
+
+class Poet(Resource):
+    @marshal_with(resource_poets)
+    def get(self, poet_id):
+        if poet_id == 999:
+            return PoetsModel.query.all()
+        # args = poetparser.parse_args()
+        poet = PoetsModel.query.filter_by(poet_id=poet_id).first()
+        return poet
+
+    @marshal_with(resource_poets)
+    @jwt_required()
+    def post(self, poet_id):
+        args = poetparser.parse_args()
+        poet = PoetsModel(poet_name=args["poet_name"], birth_date=args["birth_date"],death_date=args["death_date"])
+        db.session.add(poet)
+        db.session.commit()
+        return "inserted"
+
+    @marshal_with(resource_poets)
+    @jwt_required()
+    def put(self, poet_id):
+        args = poetparser.parse_args()
+        poet = PoetsModel.query.filter_by(poet_id=poet_id).first()
+        if poet == None:
+            poet = PoetsModel(poet_name=args["poet_name"], birth_date=args["birth_date"],death_date=args["death_date"])
+        else:
+            poet.poet_name = args["poet_name"]
+            poet.birth_date = args["birth_date"]
+            poet.death_date = args["death_date"]
+        db.session.add(poet)
+        db.session.commit()
+        return "updated"
+
+    # @marshal_with(resource_poets)
+    @jwt_required()
+    def delete(self, poet_id):
+        poet = PoetsModel.query.filter_by(poet_id=poet_id).first()
+        db.session.delete(poet)
+        db.session.commit()
+        return f"Poet with id {poet_id} has been deleted"
+
+class Register(Resource):
+    @marshal_with(resource_register)
+    def post(self):
+        args = registerparser.parse_args()
+        user = UserModel(username=args["username"], email=args["email"], password=generate_password_hash(args["password"]))
+        db.session.add(user)
+        db.session.commit()
+        return {"msg": "Created"}, 201
+
+class Auth(Resource):
+    # @marshal_with(resource_user)
+    def post(self):
+        email = request.json.get("email", None)
+        password = request.json.get("password", None)
+
+        user = UserModel.query.filter_by(email=email).first_or_404()
+        if user == None:
+            return {"msg": "Email was not found"}
+        if email != user.email or check_password_hash(user.password,password) == False:
+            return jsonify({"msg": "Bad email or password"}), 401
+        access_token = create_access_token(identity=user.username)
+        return jsonify(access_token=access_token)
+
 class UserModel(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(40), nullable=False)
 
     def __repr__(self):
         return f"User {self.username}"
 
-class PostModel(db.Model):
-    __tablename__ = 'posts'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+class PoetsModel(db.Model):
+    __tablename__ = "poets"
+    poet_id = db.Column(db.Integer, primary_key=True)
+    poet_name = db.Column(db.String(20), unique=True, nullable=False)
+    birth_date = db.Column(db.String(20), nullable=False)
+    death_date = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
-        return f"Post {self.title}"
+        return f"Poet {self.poet_name}"
+
+class PoemsModel(db.Model):
+    __tablename__ = 'poems'
+    poem_id = db.Column(db.Integer, primary_key=True)
+    poem_title = db.Column(db.String(20), nullable=False)
+    youtube_link = db.Column(db.String(80), nullable=False)
+    creator_id = db.Column(db.Integer, db.ForeignKey('poets.poet_id'), nullable=False)
+
+    def __repr__(self):
+        return f"Poem {self.poem_title}"
 # db.create_all()
 # quit()
 
 api.add_resource(User, '/user/<int:user_id>')
-api.add_resource(Post, '/post/<int:post_id>')
-
+api.add_resource(Poet, '/poet/<int:poet_id>')
+api.add_resource(Poem, '/poem/<int:poem_id>')
+api.add_resource(Auth, '/login')
+api.add_resource(Register, '/register')
 
 if __name__ == "__main__":
     app.run(debug=True)
